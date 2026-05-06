2361f42ccdf893fd7acec1cb77530cd085095556
RomanDorokhin <dorokhinroma1@gmail.com>
2026-05-07 01:37:24 +0500

    Interview: Finalizing Agent as Game Designer v10014

--- js/screens.js
+++ js/screens.js
@@ -296,7 +296,7 @@ function switchTab(tab) {
       const link = document.createElement('link');
       link.rel = 'stylesheet';
       link.crossOrigin = 'anonymous';
-      link.href = '/smolgame/agent-dist/assets/index.css?v=10013';
+      link.href = '/smolgame/agent-dist/assets/index.css?v=10014';
       document.head.appendChild(link);
 
       // Show loading indicator in agent-root while script loads
@@ -309,7 +309,7 @@ function switchTab(tab) {
       const script = document.createElement('script');
       script.type = 'module';
       script.crossOrigin = 'anonymous';
-      script.src = '/smolgame/agent-dist/assets/index.js?v=10013';
+      script.src = '/smolgame/agent-dist/assets/index.js?v=10014';
       
       script.onload = () => {
         console.log('[Agent] Script loaded successfully');
