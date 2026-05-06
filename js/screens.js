894c53a50518ddb5661e4b93cf8eef6534f086e7
RomanDorokhin <dorokhinroma1@gmail.com>
2026-05-07 02:02:07 +0500

    Deployment: Final push v10019

--- js/screens.js
+++ js/screens.js
@@ -296,7 +296,7 @@ function switchTab(tab) {
       const link = document.createElement('link');
       link.rel = 'stylesheet';
       link.crossOrigin = 'anonymous';
-      link.href = '/smolgame/agent-dist/assets/index.css?v=10018';
+      link.href = '/smolgame/agent-dist/assets/index.css?v=10019';
       document.head.appendChild(link);
 
       // Show loading indicator in agent-root while script loads
@@ -309,7 +309,7 @@ function switchTab(tab) {
       const script = document.createElement('script');
       script.type = 'module';
       script.crossOrigin = 'anonymous';
-      script.src = '/smolgame/agent-dist/assets/index.js?v=10018';
+      script.src = '/smolgame/agent-dist/assets/index.js?v=10019';
       
       script.onload = () => {
         console.log('[Agent] Script loaded successfully');
