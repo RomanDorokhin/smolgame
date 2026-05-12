59a65b82c1ccf52761f69c3eff8a4e69188ee269
RomanDorokhin <dorokhinroma1@gmail.com>
2026-05-12 23:34:00 +0500

    Total Cleanup: Removed all generated games and temporary test files from the repository to keep only the core platform.

--- zombie-survivor-clean/src/main.js
+++
@@ -1,9 +0,0 @@
-import Phaser from 'phaser';
-import { config } from './config.js';
-import { GameScene } from './scenes/GameScene.js';
-
-// Добавляем сцены
-config.scene = [GameScene];
-
-// Создаём игру
-new Phaser.Game(config);
